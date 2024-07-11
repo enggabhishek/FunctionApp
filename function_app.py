d53ef3ec5c49e367e6d68b76efa8f63fca70b3e6
enggabhishek <abhiemin@gmail.com>
2024-07-11 02:41:36 -0400

    upload again

--- function_app.py
+++ function_app.py
@@ -1,13 +1,25 @@
 import azure.functions as func
 import logging
-
+import requests
+import os
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_example")
 def http_example(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
     name = req.params.get('name')
+    token = os.environ.get("Airflow_Token")
+    deployment_url = "https://clxyvpjqq0jyj01ncg95bcz16.astronomer.run/dztkarlq"
+    dag_id = "virtual_operator"
+    
+    response = requests.post(
+                url=f"{deployment_url}/api/v1/dags/{dag_id}/dagRuns",
+                headers={"Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        },
+        data='{}'
+    )
+    # logging.info(f"Python EventGrid triggered DAG pipelin: {response.json()}")
     if not name:
         try:
             req_body = req.get_json()
@@ -20,6 +32,6 @@ def http_example(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"This HTTP triggered function executed successfully. Python EventGrid triggered DAG pipelin: {response.json()}",
              status_code=200
         )
